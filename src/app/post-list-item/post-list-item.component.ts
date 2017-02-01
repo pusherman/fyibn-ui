03d36aa5971daa0862c023ec2137ca9a6de3e587
Corey Wilson <corey@commercesignals.com>
2017-02-01 17:02:13 -0500

    pass the postId to post-item-view and retrieve details from store

--- src/app/post-list-item/post-list-item.component.ts
+++ src/app/post-list-item/post-list-item.component.ts
@@ -1,4 +1,8 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { select } from 'ng2-redux';
+import { Observable } from 'rxjs/Observable';
+
+import { Post } from '../services/post/post.reducers';
 
 @Component({
   selector: 'post-list-item',
@@ -6,11 +10,20 @@ import { Component, Input, OnInit } from '@angular/core';
   styleUrls: ['./post-list-item.component.css']
 })
 export class PostListItemComponent implements OnInit {
-  @Input() post;
+  @Input() postId;
+  @select(['posts', 'byId']) postsById$: Observable<Post[]>;
+  public post: Post;
+
   constructor() { }
 
   ngOnInit() {
+    this.postsById$
+      .subscribe(posts => {
+        this.post = posts[this.postId];
+      });
 
   }
+
+
 
 }
