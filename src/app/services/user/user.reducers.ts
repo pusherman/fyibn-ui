b96cf66e34ec500385677ccbaa717e2b658f11b7
Corey Wilson <corey@commercesignals.com>
2017-02-02 15:44:11 -0500

    created a user reducers and user interfaces

---
+++ src/app/services/user/user.reducers.ts
@@ -0,0 +1,44 @@
+import { IAction } from '../../../store';
+import { PostActions } from '../post/post.actions';
+import { AuthActions } from '../auth/auth.actions';
+
+export interface User {
+  id: number;
+  name: string;
+  email: string;
+  created_at: Date;
+  updated_at: Date;
+}
+
+export interface Users {
+  byId: {number?: User};
+  isFetching: boolean;
+  error: boolean;
+}
+
+const INITIAL_STATE: Users = {
+  byId: {},
+  isFetching: false,
+  error: false,
+};
+
+export function userReducer(
+    state: Users = INITIAL_STATE,
+    action: IAction,
+  ): Users {
+
+  switch (action.type) {
+    case PostActions.FETCH_POSTS_SUCCESSFUL:
+      return Object.assign({}, state, {
+        byId: action.payload.entities.users,
+        isFetching: false,
+        error: false,
+      });
+
+    case AuthActions.AUTH_ENDED:
+      return Object.assign({}, state, INITIAL_STATE);
+
+    default:
+      return state;
+  }
+}
--- src/store/index.ts
+++ src/store/index.ts
@@ -8,15 +8,18 @@ import { AuthActions } from '../app/services/auth/auth.actions';
 
 import { authReducer, IAuth } from '../app/services/auth/auth.reducers';
 import { postReducer, Posts } from '../app/services/post/post.reducers';
+import { userReducer, Users } from '../app/services/user/user.reducers';
 
 export interface IAppState {
   auth: IAuth;
   posts: Posts;
+  users: Users;
 };
 
 const appReducer = combineReducers<IAppState>({
   auth: authReducer,
   posts: postReducer,
+  users: userReducer,
 });
 
 export const rootReducer = (state, action) => {
