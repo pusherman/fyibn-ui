021224cb153b22cde6bef8ed6a9cfd7c6870f402
Corey Wilson <corey@commercesignals.com>
2017-02-06 15:19:17 -0500

    import the pagination component

--- src/app/post-list/post-list.module.ts
+++ src/app/post-list/post-list.module.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { MomentModule } from 'angular2-moment';
+
 import { PostListComponent } from './post-list.component';
 import { PostListItemComponent } from '../post-list-item/post-list-item.component';
 import { PostListRoutingModule } from './post-list.route';
-
+import { PaginationLinksComponent } from '../pagination-links/pagination-links.component';
 import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
@@ -17,6 +18,7 @@ import { SharedModule } from '../shared/shared.module';
   declarations: [
     PostListComponent,
     PostListItemComponent,
+    PaginationLinksComponent,
   ],
   providers: [
   ],
