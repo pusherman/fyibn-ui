b14c50d52eab14051ca21ac7b5b1f295ecfcbc93
Corey Wilson <corey@commercesignals.com>
2017-02-13 14:32:11 -0500

    make the comment form reactive

--- src/app/add-comment/add-comment.component.ts
+++ src/app/add-comment/add-comment.component.ts
@@ -1,16 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy, Input } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+import { Comment } from './add-comment.interface';
 
 @Component({
   selector: 'add-comment',
   templateUrl: './add-comment.component.html',
   styleUrls: ['./add-comment.component.css']
 })
-export class AddCommentComponent implements OnInit {
+export class AddCommentComponent implements OnInit, OnDestroy {
   @Input() postId: number;
+  public comment: FormGroup;
 
-  constructor() { }
+  constructor(
+    private formBuilder: FormBuilder,
+  ) { }
 
   ngOnInit() {
+    this.comment = this.formBuilder.group({
+      body: ['', Validators.required],
+    });
+
+    console.log(this.postId);
+  }
+
+  ngOnDestroy() {
+    console.log('destroying add comment');
+  }
+
+  onSubmit({ value, valid }: { value: Comment, valid: boolean }) {
+    if (valid) {
+      console.log('submitting', value.body, this.postId);
+      // this.actions.addComment(value.body);
+    }
   }
 
 }
