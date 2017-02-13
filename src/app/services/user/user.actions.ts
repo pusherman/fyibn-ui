3d450fd8792578662b141054a4190cbd4659650e
Corey Wilson <corey@commercesignals.com>
2017-02-13 15:08:40 -0500

    create user actions

---
+++ src/app/services/user/user.actions.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { NgRedux } from 'ng2-redux';
+import { IAppState } from '../../../store';
+
+@Injectable()
+export class UserActions {
+  static FETCH_ME_REQUESTED = 'FETCH_ME_REQUESTED';
+  static FETCH_ME_SUCCESSFUL = 'FETCH_ME_SUCCESSFUL';
+  static FETCH_ME_FAILED = 'FETCH_ME_FAILED';
+
+  constructor(private ngRedux: NgRedux<IAppState>) {}
+
+  getMe(): void {
+    this.ngRedux.dispatch({
+      type: UserActions.FETCH_ME_REQUESTED,
+    });
+  }
+}
