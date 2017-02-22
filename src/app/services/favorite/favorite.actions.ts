a97be839be94c1a445ca8de148e722e95469b212
Corey Wilson <corey@commercesignals.com>
2017-02-22 10:52:42 -0500

    added actions for removing a favorite

--- src/app/services/favorite/favorite.actions.ts
+++ src/app/services/favorite/favorite.actions.ts
@@ -8,6 +8,10 @@ export class FavoriteActions {
   static CREATE_FAVORITE_SUCCESSFUL = 'CREATE_FAVORITE_SUCCESSFUL';
   static CREATE_FAVORITE_FAILED = 'CREATE_FAVORITE_FAILED';
 
+  static REMOVE_FAVORITE_REQUESTED = 'REMOVE_FAVORITE_REQUESTED';
+  static REMOVE_FAVORITE_SUCCESSFUL = 'REMOVE_FAVORITE_SUCCESSFUL';
+  static REMOVE_FAVORITE_FAILED = 'REMOVE_FAVORITE_FAILED';
+
   constructor(private ngRedux: NgRedux<IAppState>) {}
 
   create({ post_id }): void {
@@ -16,6 +20,11 @@ export class FavoriteActions {
       payload: post_id,
     });
   }
+
+  remove({ post_id }): void {
+    this.ngRedux.dispatch({
+      type: FavoriteActions.REMOVE_FAVORITE_REQUESTED,
+      payload: post_id,
     });
   }
 }
