884ac954b98dd1f8096aaaae698ba18ff3695a31
Corey Wilson <corey@commercesignals.com>
2017-02-22 10:52:21 -0500

    import favorites module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,6 +18,7 @@ import { AuthModule } from './services/auth/auth.module';
 import { PostModule } from './services/post/post.module';
 import { UserModule } from './services/user/user.module';
 import { CommentModule } from './services/comment/comment.module';
+import { FavoriteModule } from './services/favorite/favorite.module';
 
 import { ApiService } from './services/api/api.service';
 
@@ -41,6 +42,7 @@ import { ApiService } from './services/api/api.service';
     PostModule,
     UserModule,
     CommentModule,
+    FavoriteModule,
   ],
   providers: [
     RootEpic,
